3125c26689b5255847014374e1bd7322b5de3e24
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-12 00:23:27 +0200

    Variables and Data Types
    Chap 03

---
+++ chap 03/03_Working with numbers.py
@@ -0,0 +1,25 @@
+# ---------------------------------------------------------
+# Addition in python : +
+# Subtraction in python : -
+# Multiplication in python : *
+# Division in python : /
+# The floor Division : // ( ex : 3 // 2 = 1 not 1.5 )
+# an exponential operation in python : **
+# Remainder of the division : %
+# In python, Don't use comma in math
+# float number : Any number with a decimal point ( ex : 3.25, 5.5 )
+# ---------------------------------------------------------
+
+# integer number
+
+print(2 + 3)
+print(2 * 3)
+print(2 ** 3)
+print(4 - 3)
+print(4 / 2)
+print(5 // 3)
+print(5 % 3)
+
+# float number
+print(0.2 + 0.3)
+print(0.2 / 0.3)
