a21e2ca9f4fe76c7c1d97215f35803bc45a8f981
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-12 01:16:30 +0200

    Update 07_Challenge What's the output.py

--- chap 03/07_Challenge What's the output.py
+++ chap 03/07_Challenge What's the output.py
@@ -7,7 +7,7 @@ print(message)  # This is going to be tricky ;)
 
 # Perform mathematical operations
 result = 2**3
-print("2**3 =", result)  # 2**3 = 2**3
+print("2**3 =", result)  # 2**3 = 8
 result = 5-3
 #print("5-3=", result)
 
