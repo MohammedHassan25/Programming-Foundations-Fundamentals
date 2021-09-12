05d6a06b26a3c7acc4958f43252e61d89d8a775f
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-12 09:13:46 +0200

    conclusion
    chap 06

---
+++ chap 06/01_Exploring languages.py
@@ -0,0 +1,4 @@
+# --------------------------------------------------------------------------------------------
+# Library : A grouping of variables and functions that someone else has written and verified
+# Java, Python, and C++ all inherit ideas from C
+# --------------------------------------------------------------------------------------------
